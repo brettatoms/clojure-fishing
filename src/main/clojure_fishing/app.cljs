d0749a0eafdae1d55b0cdf93e1380cc732f7055d
Brett Adams <brettadams@fastmail.com>
2021-06-06 10:18:25 -0400

    can't get format to work

--- src/main/clojure_fishing/app.cljs
+++ src/main/clojure_fishing/app.cljs
@@ -25,7 +25,8 @@
                          ;; TODO: couldn't get the project_search function to
                          ;; work but this seems fast enough for now...but can't
                          ;; search tags
-                         (.or (format "description.ilike.%%%s%%,name.ilike.%%%s%%" q q))
+                         ;; (.or (format "description.ilike.%%%s%%,name.ilike.%%%s%%" q q))
+                         (.or (str "description.ilike.%%" q "%%,name.ilike.%%" q "%%"))
                          <p!)
               data (. result -data)]
           (js/console.log data)
