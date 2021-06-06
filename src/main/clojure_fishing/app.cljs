6016d11de20b9c6f630d3c5fe589a3778124f6b8
Brett Adams <brettadams@fastmail.com>
2021-06-06 10:29:27 -0400

    no format

--- src/main/clojure_fishing/app.cljs
+++ src/main/clojure_fishing/app.cljs
@@ -6,7 +6,7 @@
             [clojure-fishing.login :as login]
             ;; [goog.string :as gstring :refer [format]]
             [goog.string :as gstring]
-            [goog.string.format]
+            ;; [goog.string.format]
             [reagent.core :as r]
             [reagent.dom :as rdom]
             [reitit.coercion.spec :as rts]
@@ -27,8 +27,8 @@
                          ;; TODO: couldn't get the project_search function to
                          ;; work but this seems fast enough for now...but can't
                          ;; search tags
-                         (.or (gstring/format "description.ilike.%%%s%%,name.ilike.%%%s%%" q q))
-                         ;; (.or (str "description.ilike.%%" q "%%,name.ilike.%%" q "%%"))
+                         ;; (.or (gstring/format "description.ilike.%%%s%%,name.ilike.%%%s%%" q q))
+                         (.or (str "description.ilike.%%" q "%%,name.ilike.%%" q "%%"))
                          <p!)
               data (. result -data)]
           (js/console.log data)
